8f068fa9cc6ade3e5ec68557d6edaec393c7b0d7
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-26 23:33:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,6 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   
-  new_string = ""
   string_array = []
   
   row_index = 0
@@ -21,7 +20,6 @@ def join_nested_strings(src)
     end
      row_index += 1
   end
-  binding.pry
   puts string_array
-  string_array
+  string_array.join(" ")
 end
